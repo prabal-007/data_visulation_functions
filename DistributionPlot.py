0c884cc4947a80035205ae55c10ad739e440c97c
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-12-05 17:59:42 +0530

    Create DistributionPlot.py

---
+++ DistributionPlot.py
@@ -0,0 +1,14 @@
+def DistributionPlot(RedFunction, BlueFunction, RedName, BlueName, Title):
+    width = 12
+    height = 10
+    plt.figure(figsize=(width, height))
+
+    ax1 = sns.distplot(RedFunction, hist=False, color="r", label=RedName)
+    ax2 = sns.distplot(BlueFunction, hist=False, color="b", label=BlueName, ax=ax1)
+
+    plt.title(Title)
+    plt.xlabel('Price (in dollars)')
+    plt.ylabel('Proportion of Cars')
+
+    plt.show()
+    plt.close()
